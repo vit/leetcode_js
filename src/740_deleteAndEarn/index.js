dc70194f94c8d9255a814f88c980a9bbace4aa6a
vit <shiegin@gmail.com>
2023-12-17 17:15:52 +0300

    m

--- src/740_deleteAndEarn/index.js
+++ src/740_deleteAndEarn/index.js
@@ -8,28 +8,37 @@ function deleteAndEarn(nums) {
         nums_weights_map[num] += num;
     }
 
+    // for(const num of nums) {
+    //     nums_weights_map[num] = num in nums_weights_map
+    //         ? nums_weights_map[num] + num
+    //         : num;
+    // }
+
     const nums_weights_arr = Object.keys(nums_weights_map)
-        .map(Number)
+        // .map(Number)
         .sort( (a, b) => a - b )
         .map( num => ({
             num,
-            weight: nums_weights_map[num]
-        }) )
-    ;
+            weight: nums_weights_map[num],
+        }) );
 
     let [prev_max, curr_max] = [0, 0];
     let prev_num = 0;
 
-    for(const elm of nums_weights_arr) {
-        const [curr_num, curr_weight] = [elm.num, elm.weight];
+    for(const { num, weight } of nums_weights_arr) {
+        const new_max = (num - prev_num == 1)
+            ? Math.max(curr_max, prev_max + weight)
+            : curr_max + weight;
 
-        const new_max = 
-            curr_num - prev_num == 1
-                ? Math.max(curr_max, prev_max + curr_weight)
-                : curr_max + curr_weight
-        ;
+        // let new_max;
+        // if(num - prev_num == 1) {
+        //     new_max = Math.max(curr_max, prev_max + weight);
+        // } else {
+        //     new_max = curr_max + weight;
+        // }
 
-        prev_num = curr_num;
+
+        prev_num = num;
         [prev_max, curr_max] = [curr_max, new_max];
     }
 
