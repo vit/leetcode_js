9c4f5a09f603bafa3796bd1ceb355d42fb30d0af
vit <shiegin@gmail.com>
2023-12-21 14:35:36 +0300

    m

---
+++ src/221_maximalSquare/index.js
@@ -0,0 +1,17 @@
+function maximalSquare(matrix) {
+
+    let maxEdge = 0;
+    for(let i=0; i<matrix.length; i++)
+        for(let j=0; j<matrix[i].length; j++) {
+            if(i>0 && j>0 && matrix[i][j] > 0)
+                matrix[i][j] = 1 + Math.min(
+                    matrix[i-1][j-1],
+                    matrix[i-1][j],
+                    matrix[i][j-1]
+                );
+            if(maxEdge < matrix[i][j]) maxEdge = matrix[i][j];
+        }
+    return maxEdge*maxEdge;
+
+};
+module.exports = maximalSquare;
