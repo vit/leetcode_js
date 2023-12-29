670284f3eed2325d7c5600f81f22a7224e04d087
vit <shiegin@gmail.com>
2023-12-29 03:41:00 +0300

    m

--- src/139_wordBreak/index.js
+++ src/139_wordBreak/index.js
@@ -1,5 +1,23 @@
 
 function wordBreak(s, wordDict) {
+    const flags = Array(s.length).fill(false);
+    for(let i=0; i<s.length; i++) {
+        for(const w of wordDict) {
+            const prev = i - w.length;
+            const base = prev + 1;
+            const prev_flag = prev < 0 || flags[prev];
+            if(base >= 0) {
+                flags[i] ||= w == s.slice(base, base + w.length) && prev_flag;
+                if(flags[i]) break;
+            }
+        }
+    }
+    return flags[s.length-1];
+};
+
+
+/*
+function wordBreak(s, wordDict) {
     const flags = Array(s.length);
 
     function wordBreakRec(ind) {
@@ -11,15 +29,19 @@ function wordBreak(s, wordDict) {
 
         flags[ind] = false;
     
-        for(const w of wordDict)
-            if(w == s.slice(ind, ind + w.length))
+        for(const w of wordDict) {
+            if(w == s.slice(ind, ind + w.length)) {
                 flags[ind] ||= wordBreakRec(ind + w.length);
+                if(flags[ind]) break;
+            }
+        }
     
         return flags[ind];
     };
     
     return wordBreakRec(0);
 };
+*/
 
 module.exports = wordBreak;
 
