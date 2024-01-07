f660c18b85ff6f1422dfb54c67cfdf0569f0a6d7
vit <shiegin@gmail.com>
2024-01-07 18:55:37 +0300

    m

--- src/516_longestPalindromicSubsequence/index.js
+++ src/516_longestPalindromicSubsequence/index.js
@@ -1,10 +1,27 @@
 
 function longestPalindromeSubseq(s) {
     const n = s.length;
+    const dp = Array(n).fill().map(_ => Array(n).fill(0));
+
+    for(let left=n-1; left>=0; left--) {
+        dp[left][left] = 1;
+        for(let right=left+1; right<n; right++) {
+            if(s[left] == s[right])
+                dp[left][right] = 2 + dp[left+1][right-1];
+            else
+                dp[left][right] = Math.max(dp[left+1][right], dp[left][right-1]);
+        }
+    }
+    return dp[0][n-1];
+}
+
+/*
+function longestPalindromeSubseq(s) {
+    const n = s.length;
     let max = 0;
 
-    const dp_equals = new Array(n).fill(0).map(_ => new Array(n).fill(0));
-    const dp_wings = new Array(n).fill(0).map(_ => new Array(n).fill(0));
+    const dp_equals = Array(n).fill().map(_ => Array(n).fill(0));
+    const dp_wings = Array(n).fill().map(_ => Array(n).fill(0));
 
     for(let left=0; left<n; left++) {
         for(let right=n-1; right>=left; right--) {
@@ -22,6 +39,7 @@ function longestPalindromeSubseq(s) {
     }
     return max;
 }
+*/
 
 module.exports = longestPalindromeSubseq;
 
