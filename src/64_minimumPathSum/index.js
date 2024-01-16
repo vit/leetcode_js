2cd6011237c7fa08d22f5dd0369db8bfe679bcd4
vit <shiegin@gmail.com>
2024-01-16 17:29:53 +0300

    m

--- src/64_minimumPathSum/index.js
+++ src/64_minimumPathSum/index.js
@@ -6,19 +6,14 @@ function minPathSum(grid) {
     const dp = Array(m).fill().map(_ => Array(n).fill(0));
 
     for(let i=0; i<m; i++) {
-        dp[i][0] = grid[i][0];
-        if(i>0) dp[i][0] += dp[i-1][0];
-    }
-    for(let j=0; j<n; j++) {
-        dp[0][j] = grid[0][j]
-        if(j>0) dp[0][j] += dp[0][j-1];
-    }
-
-    for(let i=1; i<m; i++) {
-        for(let j=1; j<n; j++) {
-            const top = dp[i-1][j];
-            const left = dp[i][j-1];
-            dp[i][j] = grid[i][j] + Math.min(top, left);
+        for(let j=0; j<n; j++) {
+            if(i==0 && j==0) {
+                dp[i][j] = grid[i][j];
+            } else {
+                const top = (i>0) ? dp[i-1][j] : Infinity;
+                const left = (j>0) ? dp[i][j-1] : Infinity;
+                dp[i][j] = grid[i][j] + Math.min(top, left);
+            }
         }
     }
 
